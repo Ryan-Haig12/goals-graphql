4beeca3c11ef0d3b9a394b898be21f49873bd098
Ryan Haig <haigryan@gmail.com>
2020-02-19 18:27:17 -0700

    changed the table name that finishedGoals write to

--- mongooseDataModels/FinishedGoal.js
+++ mongooseDataModels/FinishedGoal.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const CompletedGoalSchema = new Schema({
+const FinishedGoalSchema = new Schema({
     goalId: {
         type: String,
         required: true
@@ -28,4 +28,4 @@ const CompletedGoalSchema = new Schema({
     }
 })
 
-module.exports = CompletedGoal = mongoose.model('completedGoals', CompletedGoalSchema)
+module.exports = FinishedGoal = mongoose.model('finishedGoals', FinishedGoalSchema)
