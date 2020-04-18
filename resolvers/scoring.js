3a40a15d72b5b34e82cfe797cf513acdf20eafc9
Ryan Haig <haigryan@gmail.com>
2020-04-18 16:27:59 -0600

    minor score bug fix on which 12.5 was higher than 12.75

--- resolvers/scoring.js
+++ resolvers/scoring.js
@@ -3,6 +3,12 @@ const moment = require('moment')
 const decodeJWT = require('../utils/decodeJWT')
 const FinishedGoal = require('../mongooseDataModels/FinishedGoal')
 
+// for some reason, just one sort function is not enough to sort the data completely
+// adding a second sort function ensures that a user with 12.75 will rank above a user with 12
+// need to add another one to ensure 12.75 is ranked higher than 12.5
+// so inneficient... I need to figure out how to do this better
+const sortData = data => data.sort((a, b) => (parseInt(a.score) < parseInt(b.score)) ? 1 : -1).sort((a, b) => (parseInt(a.score) < parseInt(b.score)) ? 1 : -1).sort((a, b) => (parseInt(a.score) < parseInt(b.score)) ? 1 : -1)
+
 // take in an array of userIds, a single groupId, and 2 epoch times in milliseconds (startTime, endTime)
 // loop through every userId, grab all of their finishedGoals where finishedGoals.timeCompleted is in between the startTime and endTime,
 // for all of the finishedGoals that are in the given groupId, add the finishedGoals.point / finishedGoals.minutesLogged to the players score
@@ -39,11 +45,7 @@ const calcUserScore = async (parent, args, { userJWT }, info) => {
     })
 
     let count = 1
-    // for some reason, just one sort function is not enough to sort the data completely
-    // adding a second sort function ensures that a user with 12.75 will rank above a user with 12
-    data
-        .sort((a, b) => (parseInt(a.score) < parseInt(b.score)) ? 1 : -1)
-        .sort((a, b) => (parseInt(a.score) < parseInt(b.score)) ? 1 : -1)
+    data = sortData(data)
     data.map(d => d.rank = count++)
 
     return data
@@ -74,8 +76,7 @@ const calcUserScore_helperFunction = ({ userIds, allFinishedGoals, startTime, en
     })
 
     let count = 1
-    data.sort((a, b) => (parseInt(a.score) < parseInt(b.score)) ? 1 : -1)
-    data.sort((a, b) => (parseInt(a.score) < parseInt(b.score)) ? 1 : -1) // hacky solution to fix bug where 11.00 > 11.75
+    data = sortData(data)
     data.map(d => d.rank = count++)
 
     return data
